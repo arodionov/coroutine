1f4123ee000e97f4e54185f7cb17524ab0fcfc86
Andrii_Rodionov <Andrii_Rodionov@epam.com>
2018-02-12 17:33:44 +0200

    Initial commit

---
+++ src/main/kotlin/CoroutineApp.kt
@@ -0,0 +1,33 @@
+import com.github.kittinunf.fuel.httpGet
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+
+fun main(args: Array<String>) {
+    println("Start")
+    println(Thread.currentThread().name)
+
+    for(i in 1..10) {
+        launch {
+            println(i.toString() + " " + Thread.currentThread().name)
+            val res = long(i)
+            println(i.toString() + " " + Thread.currentThread().name)
+            println(res.toString() + " " + "Hello")
+        }
+    }
+
+    Thread.sleep(20000) // wait for 2 seconds
+    println("Stop")
+}
+
+suspend fun long(i: Int): Int {
+    //delay(1000)
+    val res = async {
+        val (request, response, result) = "http://jug.ua/".httpGet().responseString()
+        request
+    }
+    res.await()
+    return i
+}
+
+
